b46f3890969a51c237df52b3e0043c4f1ebd6462
Carlos Eduardo <carloscosta8001@gmail.com>
2026-03-05 21:26:10 -0300

    chore: gitignore adjusts and new packages

--- src/screens/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomeScreen({navigation}){
-    return (
-        <View>
-            <Text> Teste </Text>
-        </View>
-    )
-}
