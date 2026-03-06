04519bab9569e7ff6973a53d610a26e301d0a80d
Carlos Eduardo <carloscosta8001@gmail.com>
2026-03-05 21:22:15 -0300

    fix: Adjusting the nav logic

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
+
 import LoginScreen from './src/screens/LoginScreen';
+import TabNavigator from './src/components/TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,8 +10,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Main"
+          component={TabNavigator}
+          options={{ headerShown: false }}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
