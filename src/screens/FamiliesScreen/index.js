85ef58a037d6a30603b8a3f85c35180e108db2e3
Carlos Eduardo <carloscosta8001@gmail.com>
2026-03-05 21:17:12 -0300

    feat: Placeholder for Visits and Families screens

---
+++ src/screens/FamiliesScreen/index.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import styles from './style';
+export default function FamiliesScreen() {
+  return (
+    <View>
+      <Text>FAMILIAS</Text>
+    </View>
+  );
+}
---
+++ src/screens/VisitsScreen/index.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import styles from './style';
+export default function VisitsScreen() {
+  return (
+    <View>
+      <Text>VISITAS</Text>
+    </View>
+  );
+}
