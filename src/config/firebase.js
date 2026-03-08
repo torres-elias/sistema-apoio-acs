a1475f09cb023222b78187a0399422785b7bc103
MatheEduar <edutheu_dev@hotmail.com>
2026-03-08 15:10:24 -0300

    fix(.gitignore): arquivo src/config/firebase.js adicionado ao git com variáveis de ambiente

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+// Configuração obtida do seu .env
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa o Auth com persistência de dados (para o usuário não deslogar ao fechar o app)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { auth };
