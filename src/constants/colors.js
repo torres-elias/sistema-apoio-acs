23bc79d24be5a0a0eb3f9ffde0dce57b98111fc0
Carlos Eduardo <carloscosta8001@gmail.com>
2026-03-19 15:27:21 -0300

    chore: Adicinando novas cores

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,20 +1,20 @@
 const COLORS = {
-    primary: '#1565C0',
-    primaryLight: '#42A5F5',
-    primaryDark: '#0D47A1',
-
-    // Cores que eu Matheus queria
-    // primary: '#61bf52',
-    // primaryLight: '#8ddf79',
-    // primaryDark: '#36a02c',
-
-
-    white: '#f5f5ff',
-    grey: '#A0AEC0',
-    greyDark: '#64748B',
-    background: '#f4f4f4',
-    card: '#ebebeb',
-    text: '#1f2937'
-}
-
+  primary: '#1565C0',
+  primaryLight: '#42A5F5',
+  primaryDark: '#0D47A1',
+ 
+  white: '#f5f5ff',
+  grey: '#A0AEC0',
+  greyDark: '#64748B',
+  background: '#f4f4f4',
+  card: '#ebebeb',
+  text: '#1f2937',
+ 
+  textLight: '#6B7280',
+  border: '#E5E7EB',
+  danger: '#EF4444',
+  warning: '#F59E0B',
+  success: '#10B981',
+};
+ 
 export default COLORS;
